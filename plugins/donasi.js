089b524bfa570e4697a8e6d8d9315ecb20c293ad
Ramabotz9 <108044415+Ramabotz9@users.noreply.github.com>
2022-07-09 21:03:23 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,14 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0858-5053-9404]
-│ •  [0858-5053-9404]
+│ •  [0821-4756-2437]
+│ •  [0877-9323-6775]
 ╰────
+Donasi Saldo
+*GOPAY* [087793236775]
+*DANA* [BLM ADA]
+DONASI FOLLOW
+ig:ahmdzakaria47
+Tt:lagi pensi
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
